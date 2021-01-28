eca312cc58fe4b382a527211269301a0d9fe0938
Gamra01 <ilyassstudies2017@gmail.com>
2021-01-28 11:20:42 +0100

    Layout

--- ecommerce-node-react/src/core/Home.js
+++ ecommerce-node-react/src/core/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import Layout from './Layout'
 function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <Layout 
+                title="Home Page" 
+                description="Node react ecommerce" 
+                className="container">
+
+                <h2>test</h2>
+
+            </Layout>
         </div>
     )
 }
