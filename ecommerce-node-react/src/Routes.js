f812f64d1e89891121c412a7217d507ea889b214
Gamra01 <ilyassstudies2017@gmail.com>
2021-01-28 10:26:39 +0100

    Menu

--- ecommerce-node-react/src/Routes.js
+++ ecommerce-node-react/src/Routes.js
@@ -4,10 +4,11 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './core/Home';
 import Signin from './user/Signin';
 import Signup from './user/Signup';
-
+import Menu from './core/Menu';
 function Routes() {
     return (
         <BrowserRouter>
+            <Menu/>
             <Switch>
                 <Route path='/' exact component={Home}/>
                 <Route path='/signin' exact component={Signin} />
---
+++ ecommerce-node-react/src/core/Menu.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link, withRouter } from 'react-router-dom'
+
+const isActive = (history, path) => {
+
+if(history.location.pathname === path){
+        return { color: '#ffbf00'}
+    }
+    else {
+        return { color: '#fff' }
+    }
+}
+const Menu = (props) => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+                    <Link className="navbar-brand" to="/">Ecommerce</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <Link style={isActive(props.history, '/')} className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link style={isActive(props.history, '/signin')} className="nav-link" to="/signin">SignIn</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link style={isActive(props.history, '/signup')} className="nav-link disabled" to="/signup">SignUp</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+        </div>
+    )
+}
+
+export default withRouter(Menu)
